36375c5f21ee4c8046147aacf832dc998cdd852a
Venkata Reddy Bhavanam <venkatareddy.cs@gmail.com>
2014-03-23 16:06:14 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitty::Application.config.secret_token = 'a975be0eab96a927204cb9f4cb997847125ade03ecc931c6396020aba76a69be4071dd54853b3ad3da318b036e19110bc5a190596b5f09a1c65be7823eadbf18'
